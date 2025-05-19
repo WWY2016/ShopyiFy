cef81b2434a0d8d4b95d2060b01cb65f810e7aca
wwy <wangwenyi@yhbj.cn>
2025-05-19 10:51:47 +0800

    1.0.0

--- app/src/main/java/com/cx/shopify/test/ui/BaseApplication.java
+++ app/src/main/java/com/cx/shopify/test/ui/BaseApplication.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import androidx.multidex.MultiDex;
 import androidx.multidex.MultiDexApplication;
 
-import com.cx.shopify.api.Shopify;
+
 
 public class BaseApplication extends MultiDexApplication {
 
@@ -18,7 +18,7 @@ public class BaseApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        Shopify.init("5e2179c3-093b-4e6f-a476-d301ff850570");
+
     }
 
 
